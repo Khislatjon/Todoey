d3465301b601293c7cd6d88adbf6ae334211eee2
Khislatjon <valijonovxislatjon@gmail.com>
2019-11-22 18:57:50 +0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Hislat on 11/22/19.
+//  Copyright © 2019 Khislatjon. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title:String = ""
+    var done: Bool = false
+}
